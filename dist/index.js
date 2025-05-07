2c47ab7fc52d1e5e7a2e0242bfcc3b0214b26cf3
dikshapandey0201 <dikshapandey0201@gmail.com>
2025-05-07 22:11:14 +0530

    updated cors v2

--- dist/index.js
+++ dist/index.js
@@ -13,7 +13,12 @@ const cors_1 = __importDefault(require("cors"));
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: 'https://todo-client-pi-ruby.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true
+}));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_parser_1.default)());
